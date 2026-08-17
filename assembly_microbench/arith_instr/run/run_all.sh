c5f16fb2da9a60474144cc02cd7d827972b44396
Pei-Hung Lin <lin32@llnl.gov>
2026-08-17 08:49:17 -0700

    assembly microbenchmark results

--- assembly_microbench/arith_instr/run/run_all.sh
+++ assembly_microbench/arith_instr/run/run_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)
 
-"${script_dir}/run_scalar_int.sh"
-"${script_dir}/run_scalar_fp.sh"
-"${script_dir}/run_vector_int.sh"
-"${script_dir}/run_vector_fp.sh"
+script -q -e -c "${script_dir}/run_scalar_int.sh" "k3_report/k3_scalar_int.txt"
+script -q -e -c "${script_dir}/run_scalar_fp.sh"  "k3_report/k3_scalar_fp.txt"
+script -q -e -c "${script_dir}/run_vector_int.sh" "k3_report/k3_vector_int.txt"
+script -q -e -c "${script_dir}/run_vector_fp.sh"  "k3_report/k3_vector_fp.txt"
--- assembly_microbench/ldst_instr/run/run_all.sh
+++ assembly_microbench/ldst_instr/run/run_all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)
 
-"${script_dir}/run_scalar.sh"
-"${script_dir}/run_vector_contiguous.sh"
-"${script_dir}/run_vector_mask.sh"
-"${script_dir}/run_vector_setvl.sh"
-"${script_dir}/run_vector_strided.sh"
+script -q -e -c "${script_dir}/run_scalar.sh"             "k3_report/k3_scalar.txt"
+script -q -e -c "${script_dir}/run_vector_contiguous.sh"  "k3_report/k3_vector_contiguous.txt"
+script -q -e -c "${script_dir}/run_vector_mask.sh"        "k3_report/k3_vector_mask.txt"
+script -q -e -c "${script_dir}/run_vector_setvl.sh"       "k3_report/k3_vector_setvl.txt"
+script -q -e -c "${script_dir}/run_vector_strided.sh"     "k3_report/k3_vector_strided.txt"
